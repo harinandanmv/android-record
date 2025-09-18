e31717e88745c5a583b1fdbbdf1ed596fe30b5fb
M V Harinandan <harinandanmv11@gmail.com>
2025-09-18 22:18:50 +0530

    Added Exp11

---
+++ Exp11/app/src/main/java/com/example/onlinevideo/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.onlinevideo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import com.example.onlinevideo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        videoView = findViewById(R.id.videoView);
+
+        String videoUrl = "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4";
+
+        Uri uri = Uri.parse(videoUrl);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+
+        videoView.start();
+    }
+}
